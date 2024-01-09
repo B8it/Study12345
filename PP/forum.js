ae3f587975a231ad67a6f0c00104f49c53cbe168
B8it <111212133+B8it@users.noreply.github.com>
2024-01-09 12:31:39 +0700

    Add files via upload

---
+++ PP/forum.js
@@ -0,0 +1,21 @@
+ 
+	function sendReply(id){
+        x = document['form' + id]
+		reply = x.user_reply.value;
+		date = x.date.value;
+		time = x.time.value;
+		if(window.XMLHttpRequest){
+			xmlhttp = new XMLHttpRequest();
+		}else{
+			xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+		}
+		
+		xmlhttp.onreadystatechange = function(){
+			if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+				document.getElementById("div"+id).innerHTML = xmlhttp.responseText;
+			}
+		}
+		xmlhttp.open('GET', 'reply.php?reply='+reply+'&post_id='+post_id+'&user_id='+user_id+'&time='+time+'&date='+date, true);
+		xmlhttp.send();
+
+	} 
